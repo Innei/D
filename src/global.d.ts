100bcdcc20565bddb8a318f3b693f74ef002f0af
Innei <i@innei.in>
2023-11-13 21:41:36 +0800

    chore: deps
    Signed-off-by: Innei <i@innei.in>

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,3 @@
 import 'vite/client'
+
 export {}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-default-export */
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
