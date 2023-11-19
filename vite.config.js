df729f10ab6218dd4f0376e20c59ef79fb4dc319
Innei <i@innei.in>
2023-11-19 16:09:08 +0800

    fix: add favicon
    Signed-off-by: Innei <i@innei.in>

--- vite.config.js
+++ vite.config.js
@@ -14,24 +14,34 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      includeAssets: ['favicon.ico', 'favicon.png', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
 
       manifest: {
+        display: 'standalone',
+        description:
+          'A personal blog and space discussing technology, life, and more.',
+        lang: 'zh-CN',
         name: '森',
         icons: [
           {
-            src: 'favicon.png',
+            src: '192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'favicon.png',
+            src: '256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+
+          {
+            src: '512.png',
             sizes: '512x512',
             type: 'image/png',
           },
         ],
         short_name: '森',
-        theme_color: '#3498db',
+        theme_color: '#4a4a4a',
       },
     }),
   ],
